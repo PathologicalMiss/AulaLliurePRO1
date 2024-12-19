2dfd5d2584e56c1f4ba5cc3d7bee41451e128029
ToniGratacosMiralles <toni.gratacos.miralles@gmail.com>
2024-12-19 09:12:03 +0100

    Algorismes

--- Algorismes/CercaDicotomica/cd1.cpp
+++ Algorismes/CercaDicotomica/cd1.cpp
@@ -4,6 +4,7 @@
 #include <random> // Required for random_device, mt19937, shuffle
 using namespace std;
 
+// Function to generate a random vector of a given size with numbers in a specific range
 vector<int> generate_random_vector(int size, int lower_bound, int upper_bound) {
     // Create a vector with numbers from lower_bound to upper_bound-1
     vector<int> vec;
@@ -16,7 +17,7 @@ vector<int> generate_random_vector(int size, int lower_bound, int upper_bound) {
     mt19937 g(rd());     // Mersenne Twister random number generator
     shuffle(vec.begin(), vec.end(), g);  // Shuffle the vector
 
-    // Take the first 'size' elements of the shuffled vector
+    // Resize the vector to keep only the first 'size' elements
     vec.resize(size);
 
     // Sort the vector in increasing order
@@ -25,35 +26,41 @@ vector<int> generate_random_vector(int size, int lower_bound, int upper_bound) {
     return vec;
 }
 
-
+// Function to perform binary search on a sorted vector
 int dico_search(const vector<int> &v, int l, int r, int n) {
-    if (l>r) return -1;
-    int mid = (l+r/2);
-    if (v[mid] < n) return dico_search(v, l, mid, n);
-    if (v[mid] > n) return dico_search(v, mid, l, n);
-    else return mid;
-
+    if (l > r) return -1; // Base case: the element is not found
+    int mid = (l + r) / 2; // Calculate the middle index
+    if (v[mid] < n) return dico_search(v, mid + 1, r, n); // Search in the right half
+    if (v[mid] > n) return dico_search(v, l, mid - 1, n); // Search in the left half
+    return mid; // Element found at index mid
 }
-
 
 int main() {
     int size = 10;           // Desired number of elements in the vector
     int lower_bound = 1;     // Lower bound of the range
     int upper_bound = 100;   // Upper bound of the range
 
+    // Generate a sorted random vector
     vector<int> v = generate_random_vector(size, lower_bound, upper_bound);
 
+    // Print the generated vector
     for (int num : v) {
         cout << num << " ";
     }
     cout << endl;
 
-
+    // Input the number to search for
     int n;
     cin >> n;
-    int pos = dico_search(v, 0, size, n);
-    if (pos != -1) cout << "Posició de " << n << " al vector és " << pos << endl;
-    else cout << "L'element " << n << "no es troba al vector :(" << endl;
 
+    // Perform binary search
+    int pos = dico_search(v, 0, size - 1, n);
 
+    // Output the result
+    if (pos != -1) 
+        cout << "The position of " << n << " in the vector is " << pos << endl;
+    else 
+        cout << "The element " << n << " is not in the vector :(" << endl;
+
+    return 0;
 }
